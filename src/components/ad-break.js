09cc9f91e62bf62209e7b4fa72e7e161c7d7f4df
blaak <blaak@ensemble.com>
2021-02-09 13:44:34 -0800

    cleanup

--- src/components/ad-break.js
+++ src/components/ad-break.js
@@ -1,7 +1,7 @@
 /**
- * Describes a single ad break timestance that covers 1 or more fallback ad videos in the main video
- * (ads are assumed to be sitched in), that furthermore describes a true[X] interactive ad to show
- * over top of the main video when the ad break is encountered during playback.
+ * Describes a single timespan within a full video that contains consecutive stitched-in ad videos.
+ * The first video in each ad break is assumed to be the placeholder video for the true[X] interactive ad, and
+ * as such is not actually played but skipped over.
  */
 export class AdBreak {
     constructor(cuePoint, index) {
